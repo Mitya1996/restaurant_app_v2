0ed70265d553bf6a1a5aae299bff20b3f68c3e9a
mits <dmitrybaycharov@gmail.com>
2021-05-28 12:13:59 -0500

    init, works with firestore!

---
+++ main.py
@@ -0,0 +1,40 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+from flask import Flask, request, redirect
+from flask.templating import render_template
+app = Flask(__name__)
+
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': 'restaurant-app-314718',
+})
+
+db = firestore.client()
+
+
+@app.route('/')
+def home():
+    doc_ref = db.collection('restaurant').document('menu')
+
+    doc = doc_ref.get()
+    if doc.exists:
+        menu = doc.to_dict()['text']
+    else:
+        menu = ''
+
+    return render_template('index.html', menu=menu)
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    user_input = request.form['menu-textarea-input']
+
+    updated_menu = {
+        'text' : user_input
+    }
+    db.collection('restaurant').document('menu').set(updated_menu)
+
+    return redirect('/')
