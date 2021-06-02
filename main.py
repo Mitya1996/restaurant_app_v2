e2146fdafb8ef598cbc34e0a6e7341c17d78631d
mits <dmitrybaycharov@gmail.com>
2021-06-02 15:44:29 -0500

    homepage complete

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import os
 
 from flask import Flask, render_template, request, redirect
+from flask_login import LoginManager, login_user, login_required
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-from google.cloud import storage
-
+from gcs_read_images import image_urls
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev')
-CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
+GOOGLE_STORAGE_BUCKET = os.environ.get('GOOGLE_STORAGE_BUCKET', 'restaurant-app-314718-public')
 
 
 # Use the application default credentials
@@ -23,6 +23,14 @@ firebase_admin.initialize_app(cred, {
 db = firestore.client()
 
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.collection('users').document(user_id).get()
+
+
 @app.route('/')
 def home():
     doc = db.collection('restaurant').document('menu').get()
@@ -32,7 +40,8 @@ def home():
     else:
         menu = ''
 
-    return render_template('index.html', menu=menu)
+    user_image_urls = image_urls(GOOGLE_STORAGE_BUCKET, 'user-images/img')
+    return render_template('index.html', menu=menu, user_image_urls=user_image_urls)
 
 
 @app.route('/update', methods=['POST'])
