fb95a9e77c10fe2343364cee6b54ccf52b50619b
mits <dmitrybaycharov@gmail.com>
2021-06-12 11:59:42 -0500

    testing tests

--- main.py
+++ main.py
@@ -28,16 +28,10 @@ def inject_user():
 @app.route('/testing')
 def test():
     import subprocess
-    bashCmd = ["ls", "./"]
+    bashCmd = ["ls", "../runsecrets"]
     process = subprocess.Popen(bashCmd, stdout=subprocess.PIPE)
     output, error = process.communicate()
-    bashCmd2 = ["ls", "../"]
-    process = subprocess.Popen(bashCmd2, stdout=subprocess.PIPE)
-    output2, error = process.communicate()
-    bashCmd3 = ["ls", "../runsecrets"]
-    process = subprocess.Popen(bashCmd3, stdout=subprocess.PIPE)
-    output3, error = process.communicate()
-    return [output, output2, output3]
+    return output
 
 @app.route('/')
 def home():
