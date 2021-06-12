dfbd807c00ce6104dd0e3428e45e5ecdb0fc7124
mits <dmitrybaycharov@gmail.com>
2021-06-12 12:28:29 -0500

    testing tests

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def inject_user():
 @app.route('/testing')
 def test():
     import subprocess
-    bashCmd = ["cd", "../runsecrets", "&&", "cat", "GOOGLE_APPLICATION_CREDENTIALS"]
+    bashCmd = ["sudo", "cd", "../runsecrets/GOOGLE_APPLICATION_CREDENTIALS", "&&", "ls", "-la"]
     process = subprocess.Popen(bashCmd, stdout=subprocess.PIPE)
     output, error = process.communicate()
     return output
